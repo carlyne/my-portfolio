d9efe335ac5735a85f37942227b2d55cb16cab76
Carlyne Lapierre <carlyne.lapierre@gmail.com>
2020-06-04 19:13:16 +0200

    fetching project dtb with project page

--- src/views/Project/Project.vue
+++ src/views/Project/Project.vue
@@ -1,11 +1,30 @@
 <template>
-    <div class="project-view">
+    <div class="project-view container space-in">
+        <header class="container">
+            
+        </header>
         <h1> {{ $route.params.id}} </h1>
     </div>
 </template>
 
 <script>
 export default {
-	name: 'project'
+    name: 'project',
+
+    data() {
+        return {
+            projectItem: {}
+        }
+    },
+    
+    created() {
+        fetch("http://localhost:8080/projet-content.json")
+        .then(response => response.json())
+        .then((data) => {
+            return this.projectItem = data.filter(d => d.id == this.$route.params.id);
+        });
+    }
 }
 </script>
+
+<style scoped lang="scss" src="./_project-view.scss"></style>
